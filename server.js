46f75bf2db3cdb55e171b3d484e718f2131f0b26
atiqisrak <atiqisrak@gmail.com>
2023-05-14 22:15:13 +0600

    all cors allowed

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const cors = require("cors");
 
 // Import and use API routes
 const authRoutes = require("./routes/authRoutes");
